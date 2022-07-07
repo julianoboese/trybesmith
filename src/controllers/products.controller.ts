0d9a018001d55108d4ab0453b94a7313e5f05564
Juliano Boese <juliano.boese@gmail.com>
2022-07-07 19:38:55 -0300

    feat(products): :sparkles: create products controller create function
    req2

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -8,9 +8,15 @@ export default class ProductController {
     this.service = service;
   }
 
-  public getAll = async (req: Request, res: Response): Promise<void> => {
+  public getAll = async (_req: Request, res: Response): Promise<void> => {
     const products = await this.service.getAll();
 
     res.status(200).json(products);
+  };
+
+  public create = async (req: Request, res: Response): Promise<void> => {
+    const product = await this.service.create(req.body);
+
+    res.status(201).json(product);
   };
 }
