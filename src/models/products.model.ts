600b01fe38e1065dde9186fec1cb8e575b1ea0ef
Juliano Boese <juliano.boese@gmail.com>
2022-07-07 19:37:59 -0300

    feat(products): :sparkles: create products model create function
    req2

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import IProduct from '../interfaces/products.interface';
 
 export default class ProductModel {
@@ -14,5 +14,17 @@ export default class ProductModel {
     );
 
     return products as IProduct[];
+  };
+
+  public create = async (newProduct: IProduct): Promise<IProduct> => {
+    const { name, amount } = newProduct;
+    const [result] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+
+    const { insertId } = result;
+
+    return { id: insertId, ...newProduct };
   };
 }
