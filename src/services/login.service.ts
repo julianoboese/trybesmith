388e351d43901a327f11a6ba08da38ba5aaaf929
Juliano Boese <juliano.boese@gmail.com>
2022-07-08 17:28:42 -0300

    feat(login): :sparkles: create login service
    req5

---
+++ src/services/login.service.ts
@@ -0,0 +1,20 @@
+import ILogin from '../interfaces/login.interface';
+import IUser from '../interfaces/users.interface';
+import LoginModel from '../models/login.model';
+import HttpError from '../shared/http.error';
+
+export default class LoginService {
+  private model: LoginModel;
+
+  constructor(model: LoginModel) {
+    this.model = model;
+  }
+
+  public getLogin = async (login: ILogin): Promise<IUser> => {
+    const user = await this.model.getLogin(login);
+
+    if (user.length === 0) throw new HttpError(401, 'Username or password invalid');
+
+    return user[0];
+  };
+}
