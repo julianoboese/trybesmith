a51eabf69430595db97bc7c4507f127f67488644
Juliano Boese <juliano.boese@gmail.com>
2022-07-08 00:39:57 -0300

    feat(auth): :sparkles: create jwt class and its function to generate token
    req3

---
+++ src/utils/jwt.ts
@@ -0,0 +1,19 @@
+import { Secret, sign, SignOptions } from 'jsonwebtoken';
+import IUser from '../interfaces/users.interface';
+
+export default class JwtUtils {
+  private TOKEN_SECRET: Secret;
+
+  private jwtConfig: SignOptions;
+
+  constructor() {
+    this.TOKEN_SECRET = process.env.TOKEN_SECRET || 'mySecret' as Secret;
+    this.jwtConfig = {
+      expiresIn: '15m',
+      algorithm: 'HS256',
+    };
+  }
+
+  public generateJwtToken = (user: Omit<IUser, 'password'>): string => 
+    sign({ user }, this.TOKEN_SECRET, this.jwtConfig);
+}
