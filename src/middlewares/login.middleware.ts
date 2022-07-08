6af3493db1ff260952f9d83b683efad2bbc40ab4
Juliano Boese <juliano.boese@gmail.com>
2022-07-08 17:29:51 -0300

    feat(login): :sparkles: create login validation
    req5

---
+++ src/middlewares/login.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi, { ObjectSchema } from 'joi';
+import IJoiError from '../interfaces/joi.interface';
+import ValidationError from '../shared/validation.error';
+
+export default class LoginMiddleware {
+  private schema: ObjectSchema;
+
+  constructor() {
+    this.schema = Joi.object({
+      username: Joi.string().min(3).required(),
+      password: Joi.string().min(8).required(),
+    });
+  }
+
+  public validation = (req: Request, _res: Response, next: NextFunction): void => {
+    const { error } = this.schema.validate(req.body);
+
+    if (error) {
+      const joiError = error.details[0] as IJoiError;
+
+      throw new ValidationError(joiError);
+    }
+
+    next();
+  };
+}
