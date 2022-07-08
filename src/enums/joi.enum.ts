35f269b7d2a14ee4d4ea3e7b8dfe917195cf5327
Juliano Boese <juliano.boese@gmail.com>
2022-07-08 00:40:54 -0300

    feat(users): :sparkles: create user body validation
    req3

--- src/enums/joi.enum.ts
+++ src/enums/joi.enum.ts
@@ -2,6 +2,8 @@ enum JoiEnum {
   'any.required' = 400,
   'string.base' = 422,
   'string.min' = 422,
+  'number.base' = 422,
+  'number.min' = 422,
 }
 
 export default JoiEnum;
---
+++ src/middlewares/users.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi, { ObjectSchema } from 'joi';
+import JoiEnum from '../enums/joi.enum';
+import ValidationError from '../shared/validation.error';
+
+export default class UserMiddleware {
+  private schema: ObjectSchema;
+
+  constructor() {
+    this.schema = Joi.object({
+      username: Joi.string().min(3).required(),
+      classe: Joi.string().min(3).required(),
+      level: Joi.number().min(1).required(),
+      password: Joi.string().min(8).required(),
+    });
+  }
+
+  public validation = (req: Request, _res: Response, next: NextFunction): void => {
+    const { error } = this.schema.validate(req.body);
+
+    if (error) {
+      const { type, message } = error.details[0];
+
+      throw new ValidationError(type as keyof typeof JoiEnum, message);
+    }
+
+    next();
+  };
+}
