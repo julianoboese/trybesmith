2d3233e7fccb69a1d64ff31890d610cae911b23a
Juliano Boese <juliano.boese@gmail.com>
2022-07-09 09:24:28 -0300

    feat(orders): :sparkles: create POST /orders route
    req6 - DONE

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import OrderController from '../controllers/orders.controller';
+import AuthMiddleware from '../middlewares/auth.middleware';
+import OrderMiddleware from '../middlewares/orders.middleware';
 import connection from '../models/connection';
 import OrderModel from '../models/orders.model';
 import OrderService from '../services/orders.service';
@@ -10,6 +12,10 @@ const orderModel = new OrderModel(connection);
 const orderService = new OrderService(orderModel);
 const orderController = new OrderController(orderService);
 
+const orderMiddleware = new OrderMiddleware();
+const authMiddleware = new AuthMiddleware();
+
 routes.get('/', orderController.getAll);
+routes.post('/', authMiddleware.authenticate, orderMiddleware.validation, orderController.create);
 
 export default routes;
